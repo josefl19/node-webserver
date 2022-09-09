6dec848c252d9fb8bb926c399b2fe23148c71367
josefl <jose.fl_@hotmail.com>
2022-09-09 13:11:28 -0500

    Serve a full website (template)

--- app-static.js
+++ app-static.js
@@ -12,18 +12,18 @@ app.use(express.static('public'))           // No seria necesario el get a '/'
   res.send('Home page')
 })*/
 
-app.get('/elements', (req, res) => {
-    res.sendFile(path.resolve('public/elements.html'))
-})
-
-app.get('/generic', (req, res) => {
-    res.sendFile(path.resolve('public/generic.html'))
+// Get con arrow function
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo')
 })
 
 // Ruta con comodin para todas aquellas direcciones que no esten definidas
 app.get('*', (req, res) => {
-    res.send('404 | Page not found')        // get the absolute path calculation of a relative path using path.resolve()
+    res.sendFile(path.resolve('public/404.html'))        // get the absolute path calculation of a relative path using path.resolve()
 })
+
+// Forma uno de llamar al listen
+//app.listen(8080)
 
 // Listen con arrow function
 app.listen(port, () => {
