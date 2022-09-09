8c28b40157e6868388b944b380a1407d45a3f203
josefl <jose.fl_@hotmail.com>
2022-09-08 21:08:27 -0500

    Request and Response

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import * as http from 'http';
 
 http.createServer( (req, res) => {
-    res.write('Hola Mundo');
+
+    // Devolver un archivo CSV
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, { 'Content-Type': 'application/csv'});
+    
+    res.write('id, name\n');
+    res.write('1, Jose\n');
+    res.write('2, Mariana\n');
+    res.write('3, Raúl\n');
+    res.write('4, Sandra\n');
     res.end();
+    
 }).listen( 8080 );
 
 console.log('Escuchando en puerto ', 8080);
